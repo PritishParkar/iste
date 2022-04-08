b83cf33398654bc66a784b6e3f2a13283c1785b6
Pritish Parashar Parkar <pritishparkar@gmail.com>
2022-04-08 16:29:29 +0530

    First Commit

---
+++ components/NavbarLogo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NuxtLink to="/">
+      <!-- <a
+        class="text-black font-bold text-3xl"
+        style="font-family: 'Monoton', cursive"
+        ><span class="text-red-500">ISTE</span>&nbsp; PHCET</a
+      > -->
+      <img src="http://www.isteonline.in/icons/logo.png" height="70" width="70">
+      </NuxtLink
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Monoton&display=swap");
+</style>
